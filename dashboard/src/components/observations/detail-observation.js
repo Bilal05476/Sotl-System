0b56f0a1cea706f0768173a798c0b92ba655e169
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2023-04-14 12:32:59 +0500

    udpates

--- dashboard/src/components/observations/detail-observation.js
+++ dashboard/src/components/observations/detail-observation.js
@@ -1,54 +1,302 @@
-import React, { Fragment } from "react";
-import { Container } from "reactstrap";
+import React, { Fragment, useEffect, useState } from "react";
+import { Col, Container, Row, Table } from "reactstrap";
+import { useParams } from "react-router-dom";
 import Breadcrumb from "../common/breadcrumb";
+import { Eye, Loader } from "react-feather";
+import { useStateValue } from "../../StateProvider";
 
 const Detail_observation = () => {
+  const { id } = useParams();
+  const [isOpen, setIsOpen] = useState("--");
+  const [{ user }] = useStateValue();
+  const [obsDetail, setObsDetail] = useState("");
+  useEffect(() => {
+    const [obs] = user.observations.filter((item) => item.id === Number(id));
+    setObsDetail(obs);
+  }, []);
+  console.log(obsDetail);
+  // return null;
   return (
     <Fragment>
       <Breadcrumb title="Detail Observation" parent="Observations" />
-      <Container fluid={true}>
-        <div className="accordion" id="accordionExample">
-          {[
-            "Observation Scheduling",
-            "Informed Observation",
-            "Post Informed Meeting",
-            "Uninformed Observation",
-            "Professional Development Plan",
-          ].map((item) => (
-            <div className="accordion-item mb-5">
-              <h2 className="accordion-header" id="headingOne">
-                <button
-                  className="accordion-button text-light"
-                  style={{
-                    backgroundColor: "#040b5b",
-                    outline: "none",
-                    boxShadow: "none",
-                  }}
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
-                  {item}
-                </button>
-              </h2>
-              {/* <div> */}
-              <div
-                //   className="accordion-body"
-                id="collapseOne"
-                className="accordion-body accordion-collapse collapsed"
-                //collapsed show
-                aria-labelledby="headingOne"
-                data-bs-parent="#accordionExample"
+      {obsDetail && (
+        <Container fluid={true}>
+          <Row className="mb-3">
+            <Col className="xl-25">
+              {" "}
+              <span style={{ fontWeight: "500" }}>Faculty:</span>{" "}
+              {obsDetail?.faculty.name}
+            </Col>
+            <Col className="xl-25">
+              {" "}
+              <span style={{ fontWeight: "500" }}>Observer:</span>{" "}
+              {obsDetail?.observer.name}
+            </Col>
+            <Col className="xl-25 text-right">
+              <span style={{ fontWeight: "500" }}>Head of Department:</span>{" "}
+              {obsDetail?.hod.name}
+            </Col>
+            <Col className="xl-25 text-right">
+              <span style={{ fontWeight: "500" }}>Progress:</span>{" "}
+              {obsDetail?.observationProgress}%
+            </Col>
+          </Row>
+          <div className="accordion">
+            <div className="accordion-item overflow-hidden mb-5">
+              <button
+                className="btn btn-block text-light"
+                onClick={() => setIsOpen("scheduling")}
+                style={{
+                  backgroundColor: "#040b5b",
+                  outline: "none",
+                  boxShadow: "none",
+                  padding: "15px",
+                  width: "100%",
+                  borderBottomLeftRadius: "0",
+                  borderBottomRightRadius: "0",
+                }}
+                type="button"
+                aria-expanded="true"
               >
-                <strong>Ruko zara, sabar karo!</strong>
-              </div>
-              {/* </div> */}
+                Observation Scheduling
+              </button>
+              {obsDetail?.obsRequest ? (
+                <>
+                  {isOpen === "scheduling" && (
+                    <div className="accordion-body px-2 user-status table-responsive latest-order-table">
+                      <Table borderless>
+                        <thead>
+                          <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Course</th>
+                            <th scope="col">TimeSlot (O)</th>
+                            <th scope="col">TimeSlots (F)</th>
+                            <th scope="col">Artifact</th>
+                            <th scope="col">Teaching Plan (F)</th>
+                            <th scope="col">Teaching Plan (O)</th>
+                            <th scope="col">Reflection Plan (F)</th>
+                            <th scope="col">Reflection Plan (O)</th>
+                            <th scope="col">Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>{obsDetail?.obsRequest.id}</td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.course
+                                ? obsDetail?.obsRequest.course
+                                : "--"}
+                            </td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.timeSlotsByFaculty.map(
+                                (item) => `${item} `
+                              )}
+                            </td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.timeSlotsByObserver.map(
+                                (item) => `${item} `
+                              )}
+                            </td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.artifcats}
+                            </td>
+
+                            <td className="digits">
+                              {obsDetail?.obsRequest.teachingPlanByFaculty}
+                            </td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.teachingPlanByObserver}
+                            </td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.refelectionPlanByFaculty
+                                ? obsDetail?.obsRequest.refelectionPlanByFaculty
+                                : "--"}
+                            </td>
+                            <td className="digits">
+                              {obsDetail?.obsRequest.refelectionPlanByObserver}
+                            </td>
+                            <td
+                              className={`digits ${
+                                obsDetail?.obsRequest.status === "Completed"
+                                  ? "text-success"
+                                  : "text-danger"
+                              }`}
+                            >
+                              {obsDetail?.obsRequest.status}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </div>
+                  )}
+                </>
+              ) : (
+                <>
+                  {isOpen === "scheduling" && (
+                    <div className="accordion-body text-center">
+                      <strong>No data!</strong>
+                    </div>
+                  )}
+                </>
+              )}
             </div>
-          ))}
-        </div>
-      </Container>
+          </div>
+
+          <div className="accordion">
+            <div className="accordion-item overflow-hidden mb-5">
+              <button
+                className="btn btn-block text-light"
+                onClick={() => setIsOpen("informed")}
+                style={{
+                  backgroundColor: "#040b5b",
+                  outline: "none",
+                  boxShadow: "none",
+                  padding: "15px",
+                  width: "100%",
+                  borderBottomLeftRadius: "0",
+                  borderBottomRightRadius: "0",
+                }}
+                type="button"
+                aria-expanded="true"
+              >
+                Informed Observation
+              </button>
+              {obsDetail?.meetings.informedObservation ? (
+                <>
+                  {isOpen === "informed" && (
+                    <div className="accordion-body px-2 user-status table-responsive latest-order-table">
+                      <Table borderless>
+                        <thead>
+                          <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">
+                              Rubric Score{" "}
+                              <span
+                                style={{ color: "gray", fontSize: "0.8rem" }}
+                              >
+                                (will be finalize after post informed meeting)
+                              </span>
+                            </th>
+                            <th scope="col">Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td className={"digits"}>
+                              {obsDetail?.meetings.informedObservation.id}
+                            </td>
+                            <td className={"digits"}>
+                              {
+                                obsDetail?.meetings.informedObservation
+                                  .finalScore
+                              }
+                            </td>
+                            <td
+                              className={`digits ${
+                                obsDetail?.meetings.informedObservation
+                                  .status === "Completed"
+                                  ? "text-success"
+                                  : "text-primary"
+                              }`}
+                            >
+                              {obsDetail?.meetings.informedObservation.status}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </div>
+                  )}
+                </>
+              ) : (
+                <>
+                  {isOpen === "informed" && (
+                    <div className="accordion-body text-center">
+                      <strong>No data!</strong>
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+          <div className="accordion">
+            <div className="accordion-item overflow-hidden mb-5">
+              <button
+                className="btn btn-block text-light"
+                onClick={() => setIsOpen("post")}
+                style={{
+                  backgroundColor: "#040b5b",
+                  outline: "none",
+                  boxShadow: "none",
+                  padding: "15px",
+                  width: "100%",
+                  borderBottomLeftRadius: "0",
+                  borderBottomRightRadius: "0",
+                }}
+                type="button"
+                aria-expanded="true"
+              >
+                Post Informed Meeting
+              </button>
+              {obsDetail?.meetings.postObservation ? (
+                <>
+                  {isOpen === "post" && (
+                    <div className="accordion-body">
+                      <strong>Ruko zara, sabar karo!</strong>
+                    </div>
+                  )}
+                </>
+              ) : (
+                <>
+                  {isOpen === "post" && (
+                    <div className="accordion-body text-center">
+                      <strong>No data!</strong>
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+          <div className="accordion">
+            <div className="accordion-item overflow-hidden mb-5">
+              <button
+                className="btn btn-block text-light"
+                onClick={() => setIsOpen("uninformed")}
+                style={{
+                  backgroundColor: "#040b5b",
+                  outline: "none",
+                  boxShadow: "none",
+                  padding: "15px",
+                  width: "100%",
+                  borderBottomLeftRadius: "0",
+                  borderBottomRightRadius: "0",
+                }}
+                type="button"
+                aria-expanded="true"
+              >
+                Uninformed Observation
+              </button>
+              {obsDetail?.meetings.uninformedObservation ? (
+                <>
+                  {isOpen === "uninformed" && (
+                    <div className="accordion-body">
+                      <strong>Ruko zara, sabar karo!</strong>
+                    </div>
+                  )}
+                </>
+              ) : (
+                <>
+                  {isOpen === "uninformed" && (
+                    <div className="accordion-body text-center">
+                      <strong>No data!</strong>
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+        </Container>
+      )}
+      {!obsDetail && <Loader />}
     </Fragment>
   );
 };
