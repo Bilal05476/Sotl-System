faec0f643154486e2f2262bde7c3ed6ca12ea8d5
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2023-04-21 23:01:22 +0500

    updates

--- prisma-server/routes/observation.js
+++ prisma-server/routes/observation.js
@@ -1,24 +1,22 @@
 import express from "express";
 const obsRoutes = express.Router();
+// import {
+//   getAllObs,
+//   getObs,
+//   initiate,
+// } from "../controller/observation/initiateObs.js";
 import {
-  getAllObs,
-  getObs,
-  initiate,
-} from "../controller/observation/initiateObs.js";
-import {
-  // preObsAcceptedByObserver,
-  // preObsByFaculty,
   obsScheculeCreate,
   obsScheduleCycle,
-  // preObsByObserver,
 } from "../controller/observation/preObs.js";
-import { protectInitiateObs } from "../middleware/initiate.js";
+// import { protectInitiateObs } from "../middleware/initiate.js";
 
-obsRoutes.route("/observation/initiate").post(protectInitiateObs, initiate);
+// obsRoutes.route("/observation/initiate").post(protectInitiateObs, initiate);
 obsRoutes.route("/observation/scheduling").post(obsScheculeCreate);
-obsRoutes.route("/observation/scheduling/:id").post(obsScheduleCycle);
 
-obsRoutes.route("/observations").get(getAllObs);
-obsRoutes.route("/observation/:id").get(getObs);
+obsRoutes.route("/observation/scheduling/:id").put(obsScheduleCycle);
+
+// obsRoutes.route("/observations").get(getAllObs);
+// obsRoutes.route("/observation/:id").get(getObs);
 
 export default obsRoutes;
