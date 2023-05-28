d721e3d5077983eb11cbb8c21ac4bfff2a3ee6e7
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2023-05-28 21:53:44 +0500

    udpates

--- dashboard/src/routes/index.js
+++ dashboard/src/routes/index.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "../components/auth/login";
 import LayoutRoutes from "./LayoutRoutes";
 import { useStateValue } from "../StateProvider";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 const URL = process.env.PUBLIC_URL;
 
 const Routers = () => {
@@ -10,14 +12,25 @@ const Routers = () => {
 
   return (
     <Fragment>
-      <Routes>
+      <AnimatedRoutes>
         {user ? (
           <Route path={`/*`} element={<LayoutRoutes />} />
         ) : (
           <Route exact path={`${URL}/*`} element={<Login />} />
         )}
-      </Routes>
+      </AnimatedRoutes>
     </Fragment>
+  );
+};
+
+const AnimatedRoutes = ({ children }) => {
+  const location = useLocation();
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Routes location={location}>{children}</Routes>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
